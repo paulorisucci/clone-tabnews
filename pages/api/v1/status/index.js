ea4c60f6437ae53c1ab7ec04939fbf2dd3b11421
Paulo Risucci <paulorisucci@gmail.com>
2025-09-11 23:53:53 -0300

    handles database SSL connection with Self-Signed Certificate

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,7 @@
 import database from "infra/database.js"
 
 async function status(request, response) {
+
   const updatedAt = new Date().toISOString();
 
   const version = parseInt((await database.query("SHOW server_version;")).rows[0].server_version);
