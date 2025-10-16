f762fbfea8365e51adff2fc6e76ae1592a809dce
Paulo Risucci <paulorisucci@gmail.com>
2025-10-15 23:11:00 -0300

    Adding ESLint for code quality analysis

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+import pluginJest from "eslint-plugin-jest";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js, jest: pluginJest },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+        ...pluginJest.environments.globals.globals,
+      },
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat["jsx-runtime"],
+]);
