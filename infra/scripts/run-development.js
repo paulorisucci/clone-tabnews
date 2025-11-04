df94d0a02905992050e966d566d76f614b4a5758
Paulo Risucci <paulorisucci@gmail.com>
2025-11-04 08:33:43 -0300

    build: add run-development script

---
+++ infra/scripts/run-development.js
@@ -0,0 +1,63 @@
+const { spawn } = require("node:child_process");
+
+const isWindows = process.platform === "win32";
+
+async function main() {
+  try {
+    let asyncFunction;
+
+    console.log("🚀 Starting services...");
+    asyncFunction = await runAsync("npm", ["run", "services:up"]);
+
+    console.log("⏳ Waiting for database...");
+    asyncFunction = await runAsync("npm", ["run", "services:wait:database"]);
+
+    console.log("📦 Running migrations...");
+    asyncFunction = await runAsync("npm", ["run", "migrations:up"]);
+
+    console.log("💻 Starting Next.js dev server...");
+    asyncFunction = spawn("next", ["dev"], {
+      stdio: "inherit",
+      shell: isWindows,
+    });
+
+    process.on("SIGINT", async () => {
+      console.log("\n Caught CTRL+C. Stopping services...");
+      asyncFunction.kill("SIGINT");
+    });
+
+    asyncFunction.on("exit", async (code, signal) => {
+      console.log(`\nnext dev exited (code=${code}, signal=${signal})`);
+      console.log("🧹 Cleaning up...");
+
+      try {
+        await runAsync("npm", ["run", "services:stop"]);
+      } catch (e) {
+        console.error("⚠️ Error stopping services:", e.message);
+      }
+
+      console.log("✅ Cleanup finished.");
+
+      process.exit(code ?? 0);
+    });
+  } catch (error) {
+    console.error("❌ Error in setup:", error.message);
+    await runAsync("npm", ["run", "services:stop"]);
+    process.exit(1);
+  }
+}
+
+function runAsync(command, args = []) {
+  return new Promise((resolve, reject) => {
+    const proc = spawn(command, args, { stdio: "inherit", shell: isWindows });
+    proc.on("exit", (code, signal) => {
+      if (code === 0 || signal === "SIGINT") {
+        resolve();
+      } else {
+        reject(new Error(`${command} exited with code ${code}`));
+      }
+    });
+  });
+}
+
+main();
